ec046de085d9afcf195f411912f28938c0a398a8
박기정 <parkgijeoung@baggijeong-ui-MacBookPro.local>
2026-03-26 17:54:08 +0900

    Add contacts app files

---
+++ 1_basci/07_컴포넌트심화/contacts-app-practice/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app" class="container-lg">
+    <div class="card card-body bg-light">
+      <div class="title">우리반 Contact ❤️</div>
+    </div>
+
+    <!-- 연락처 입력 폼 -->
+    <ContactForm @add-contact="addContact" />
+    <!-- 연락처 목록 -->
+    <ContactList :contacts="contacts" @delete-contact="deleteContact" />
+  </div>
+</template>
+
+<script>
+import ContactList from './components/ContactList.vue';
+import ContactForm from './components/ContactForm.vue';
+export default {
+  name: 'App',
+  components: {
+    ContactList,
+    ContactForm,
+  },
+
+  data() {
+    return {
+      newContact: {
+        name: '',
+        gender: '',
+        age: '',
+        email: '',
+        github: '',
+      },
+      contacts: [
+        {
+          id: 1,
+          name: '홍길동',
+          gender: '남',
+          age: 20,
+          email: 'hong@example.com',
+          github: 'hongGD',
+          isEditing: false,
+        },
+        {
+          id: 2,
+          name: '짱구',
+          gender: '남',
+          age: 7,
+          email: 'janggu@example.com',
+          github: 'janggu',
+          isEditing: false,
+        },
+        {
+          id: 3,
+          name: '유리',
+          gender: '여',
+          age: 7,
+          email: 'yuri@example.com',
+          github: 'yuri',
+          isEditing: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addContact(id) {
+      this.contacts.push({
+        name: id.name,
+        gender: id.gender,
+        age: id.age,
+        email: id.email,
+        github: id.github,
+      });
+    },
+    deleteContact(id) {
+      let index = this.contacts.findIndex((item) => id === item.id);
+      this.contacts.splice(index, 1);
+    },
+    /* contact 추가 */
+    /* contact 삭제 */
+  },
+};
+</script>
---
+++ 1_basci/07_컴포넌트심화/contacts-app-practice/src/components/ContactForm.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="input-form card card-body mt-3">
+    <div class="row g-3" @keyup.enter="addContact">
+      <div class="col-md-2">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="이름"
+          v-model.trim="con.name"
+        />
+      </div>
+      <div class="col-md-2">
+        <select class="form-select" v-model.trim="con.gender">
+          <option value="">성별</option>
+          <option value="남">남</option>
+          <option value="여">여</option>
+        </select>
+      </div>
+      <div class="col-md-2">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="나이"
+          v-model.trim="con.age"
+        />
+      </div>
+      <div class="col-md-3">
+        <input
+          type="email"
+          class="form-control"
+          placeholder="이메일"
+          v-model.trim="con.email"
+        />
+      </div>
+      <div class="col-md-2">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="github ID"
+          v-model.trim="con.github"
+        />
+      </div>
+      <div class="col-md-1">
+        <button class="btn btn-primary w-100" @click="addContact">추가</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+
+  data() {
+    return {
+      con: {
+        name: '',
+        gender: '',
+        age: '',
+        email: '',
+        github: '',
+      },
+    };
+  },
+
+  methods: {
+    // 이름이 입력 안된 경우 '이름을 입력하세요' 알림창
+    // 이메일이 입력 안된 경우 '이메일을 입력하세요' 알림창
+    // 둘 다 입력이 된 경우 연락처 추가
+    addContact(e) {
+      if (this.con.name.length == 0) {
+        alert('이름을 입력하세요');
+        return;
+      }
+      if (this.con.email.length == 0) {
+        alert('이메일을 입력하세요');
+        return;
+      }
+      if (this.con.age <= 0) {
+        alert('정확한 나이를 기입하십시오.');
+        return;
+      }
+      this.$emit('add-contact', this.con);
+      this.con = '';
+    },
+  },
+};
+</script>
