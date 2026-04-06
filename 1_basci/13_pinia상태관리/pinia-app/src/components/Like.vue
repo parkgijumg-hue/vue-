e5ad2fff458f95d755927eb8a2e7d09ed649ab3a
박기정 <parkgijumg@gmail.com>
2026-04-06 16:43:42 +0900

    추가 업데이트

--- 1_basci/13_pinia상태관리/pinia-app/src/components/Like.vue
+++ 1_basci/13_pinia상태관리/pinia-app/src/components/Like.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <button class="like" , @click="increase">좋아요 누르기</button> <br />
-    <button class="like" , @click="decrease">좋아요 감소</button>
+    <button class="like" , @click="i.increase">좋아요 누르기</button> <br />
+    <button class="like" , @click="i.decrease">좋아요 감소</button>
   </div>
 </template>
 
@@ -9,8 +9,11 @@
 import { ref } from 'vue';
 import { useLikeStore } from '@/stores/01_basic/like';
 
-const { increase, decrease } = useLikeStore(); // like.js 에 있는 increase, decrease 메소드를 호출함
+// const { increase, decrease } = useLikeStore(); // like.js 에 있는 increase, decrease 메소드를 호출함
 // store 안에서 {} 안에있는 객체,함수 꺼내서 담기
+
+const i = useLikeStore();
+// useLikeSotre() 자체를 i에 호출하기
 </script>
 
 <style scoped></style>
